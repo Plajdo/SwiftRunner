e16f70ebcd6455d1ef97e681dc172e7230dfd37d
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-06-30 18:46:51 +0200

    something i guess

---
+++ AnotherCamera/AnotherCamera/AnotherCamera/CameraController.swift
@@ -0,0 +1,15 @@
+//
+//  CameraController.swift
+//  AnotherCamera
+//
+//  Created by Filip Šašala on 30/06/2018.
+//  Copyright © 2018 plajdo. All rights reserved.
+//
+
+import UIKit
+
+class CameraController : UIViewController{
+	
+	
+	
+}
