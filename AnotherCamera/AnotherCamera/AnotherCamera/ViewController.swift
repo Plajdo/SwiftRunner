9901e6b515c894dfa41d341132722699cd3974fd
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-06-30 21:31:05 +0200

    Working camera test app

--- AnotherCamera/AnotherCamera/AnotherCamera/ViewController.swift
+++ AnotherCamera/AnotherCamera/AnotherCamera/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+	@IBOutlet var imageView: UIImageView!
+	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,10 +21,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func onCameraClick(_ sender: UIButton) {
-        
+    @IBAction func takePhoto(_ sender: UIButton) {
+		if(UIImagePickerController.isSourceTypeAvailable(.camera)){
+			let imagePickerController = UIImagePickerController()	//New picker instance
+			imagePickerController.delegate = self					//Class that can use the controller
+			imagePickerController.sourceType = .camera				//Source - camera or library
+			
+			self.present(imagePickerController, animated: true, completion: nil)
+		}else{
+			let alert = UIAlertController(title: "Error", message: "Camera not available.", preferredStyle: .alert)
+			alert.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+			self.present(alert, animated: true, completion: nil)
+		}
+		
     }
+	
+	func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+		let image : UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+		imageView.image = image
+		picker.dismiss(animated: true, completion: nil)
+	}
+	
+	func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+		picker.dismiss(animated: true, completion: nil)
+	}
     
-
 }
 
