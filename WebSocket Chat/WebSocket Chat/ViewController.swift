f9aa5b7d0b2164dcfb6cb2a9035edfc040809b2d
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-07-15 19:41:52 +0200

    table works & project is not broken

--- WebSocket Chat/WebSocket Chat/ViewController.swift
+++ WebSocket Chat/WebSocket Chat/ViewController.swift
@@ -12,26 +12,33 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var table: UITableView!
     @IBOutlet var textField: UITextField!
+    
     var messages : [String] = ["hehe"]
+    var clientInsert : Bool = true
+    
+    let cellIdentifier : String = "MessageCell"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        insertNewMessage()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+        table.delegate = self
+        table.dataSource = self
         table.tableFooterView = UIView(frame: CGRect.zero)
+        table.rowHeight = UITableViewAutomaticDimension
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     @IBAction func buttonOnClick(_ sender: UIButton) {
-        insertNewMessage()
+        insertNewMessage(fromServer: false)
         textField.text = ""
     }
     
-    func insertNewMessage() {
+    func insertNewMessage(fromServer: Bool) {
         print(textField.text!)
         messages.append(textField.text!)
         
@@ -40,38 +47,58 @@ class ViewController: UIViewController, UITextFieldDelegate {
         table.insertRows(at: [indexPath], with: .automatic)
         table.endUpdates()
         table.scrollToRow(at: indexPath, at: .bottom, animated: true)
+        
     }
     
 }
 
+/*
+ * ViewController extension
+ * Code that happens when something happens (?)
+ */
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
+    //getRowCount
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
     
+    //getCell(cellIndex)
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let message = messages[indexPath.row]
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell")
-        //let cell = tableView.visibleCells[tableView.visibleCells.count - 1]
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
         cell!.textLabel!.text = message
+        
+        if(clientInsert){
+            cell!.backgroundColor = UIColor.blue
+            cell!.textLabel!.textColor = UIColor.white
+        }else{
+            cell!.backgroundColor = UIColor.green
+            cell!.backgroundColor = UIColor.white
+        }
         
         return cell!
         
     }
     
+    //isEditable(cellIndex)
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
     
+    //cellEdited(editingStyle, cellIndex)
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        
         if(editingStyle == .delete){
             messages.remove(at: indexPath.row)
             tableView.beginUpdates()
             tableView.deleteRows(at: [indexPath], with: .automatic)
             tableView.endUpdates()
         }
+        
+    }
+    
+    //cellSelected(cellIndex)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
     }
     
