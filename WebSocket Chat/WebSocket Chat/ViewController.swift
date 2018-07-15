74932b5cb47272151a2cafb8d71ae95eb2b66a1d
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-07-15 19:29:25 +0200

    Revert "tableworks"
    This reverts commit 17326f31d5ebe58ab0847665656ca0fcaaf0795e.

--- WebSocket Chat/WebSocket Chat/ViewController.swift
+++ WebSocket Chat/WebSocket Chat/ViewController.swift
@@ -7,101 +7,72 @@
 //
 
 import UIKit
-import Starscream
 
 class ViewController: UIViewController, UITextFieldDelegate {
-	
-	@IBOutlet var table: UITableView!
-	@IBOutlet var textField: UITextField!
-	
-	var messages : [String] = ["hehe"]
-	var clientInsert : Bool = true
-	
-	let cellIdentifier : String = "MessageCell"
-	
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		table.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
-		table.delegate = self
-		table.dataSource = self
-		table.tableFooterView = UIView(frame: CGRect.zero)
-		table.rowHeight = UITableViewAutomaticDimension
-		
-	}
-	
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-	
-	@IBAction func buttonOnClick(_ sender: UIButton) {
-		insertNewMessage(fromServer: false)
-		textField.text = ""
-	}
-	
-	func insertNewMessage(fromServer: Bool) {
-		print(textField.text!)
-		messages.append(textField.text!)
-		
-		table.beginUpdates()
-		let indexPath = IndexPath(row: messages.count - 1, section: 0)
-		table.insertRows(at: [indexPath], with: .automatic)
-		table.endUpdates()
-		table.scrollToRow(at: indexPath, at: .bottom, animated: true)
-		
-	}
-	
+    
+    @IBOutlet var table: UITableView!
+    @IBOutlet var textField: UITextField!
+    var messages : [String] = ["hehe"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        insertNewMessage()
+        table.tableFooterView = UIView(frame: CGRect.zero)
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func buttonOnClick(_ sender: UIButton) {
+        insertNewMessage()
+        textField.text = ""
+    }
+    
+    func insertNewMessage() {
+        print(textField.text!)
+        messages.append(textField.text!)
+        
+        table.beginUpdates()
+        let indexPath = IndexPath(row: messages.count - 1, section: 0)
+        table.insertRows(at: [indexPath], with: .automatic)
+        table.endUpdates()
+        table.scrollToRow(at: indexPath, at: .bottom, animated: true)
+    }
+    
 }
 
-/*
-* ViewController extension
-* Code that happens when something happens (?)
-*/
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
-	
-	//getRowCount
-	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return messages.count
-	}
-	
-	//getCell(cellIndex)
-	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		let message = messages[indexPath.row]
-		let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
-		cell!.textLabel!.text = message
-		
-		if(clientInsert){
-			cell!.backgroundColor = UIColor.blue
-			cell!.textLabel!.textColor = UIColor.white
-		}else{
-			cell!.backgroundColor = UIColor.green
-			cell!.backgroundColor = UIColor.white
-		}
-		
-		return cell!
-		
-	}
-	
-	//isEditable(cellIndex)
-	func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-		return true
-	}
-	
-	//cellEdited(editingStyle, cellIndex)
-	func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-		if(editingStyle == .delete){
-			messages.remove(at: indexPath.row)
-			tableView.beginUpdates()
-			tableView.deleteRows(at: [indexPath], with: .automatic)
-			tableView.endUpdates()
-		}
-		
-	}
-	
-	//cellSelected(cellIndex)
-	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		
-	}
-	
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let message = messages[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell")
+        //let cell = tableView.visibleCells[tableView.visibleCells.count - 1]
+        cell!.textLabel!.text = message
+        
+        return cell!
+        
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if(editingStyle == .delete){
+            messages.remove(at: indexPath.row)
+            tableView.beginUpdates()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            tableView.endUpdates()
+        }
+        
+    }
+    
 }
-
