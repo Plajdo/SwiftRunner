bfecdc9b49f16838234c3e00a5325576d1dfe7de
Plajdo <31418257+Plajdo@users.noreply.github.com>
2018-07-26 16:35:00 +0200

    LoginTest first commit

---
+++ ShardbytesLoginTest/ShardbytesLoginTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShardbytesLoginTest
+//
+//  Created by Filip Šašala on 26/07/2018.
+//  Copyright © 2018 plajdo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
